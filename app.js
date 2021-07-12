e2f34eb76c416151d3b57d886177e7292e4d920f
Lixian <weiyuanleeif@gmail.com>
2021-07-12 10:54:09 -0400

    start of project

---
+++ app.js
@@ -0,0 +1,18 @@
+let headerEl = document.getElementById("header-el")
+let cardEl = document.getElementById("card-el")
+let sumEl = document.getElementById("sum-el")
+let startBtn = document.getElementById("start-btn")
+let newCardBtn = document.getElementById("new-card-btn")
+
+function renderGame(){
+    startGame()
+}
+function startGame(){
+//when button is clicked. print card numbers & the sum they produce
+//determine you lose or win
+//change text to show winning 
+
+}
+function newCard() {
+
+}
